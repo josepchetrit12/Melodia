32b37b1c314d9f908612903edb08ad84a23156b0
Josep Chetrit <jchetrit1999@gmail.com>
2022-08-05 15:30:51 -0400

    Adding the scripts for Zora and Livepeer #2

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      forking: {
+        url: process.env.RINKEBY_RPC_URL
+      }
+    }
+  }
+
 };
