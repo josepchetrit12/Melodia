32dba5c5243ec487da50846546d4ffcfd7a14adb
Iuri <iuri.19.05@gmail.com>
2022-08-06 12:35:31 +0200

    feat: Implement Rainbow wallet #14

---
+++ packages/react-app/src/components/ui/Wallet.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider, ConnectButton } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+// React Icons
+import { AiOutlineWallet } from 'react-icons/ai';
+
+const Wallet = () => {
+    
+    const { chains, provider } = configureChains(
+        [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, chain.polygonMumbai, chain.rinkeby, chain.localhost, chain.hardhat],
+        [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()]
+    );
+  
+    const { connectors } = getDefaultWallets({
+        appName: "My RainbowKit App",
+        chains,
+    });
+  
+    const wagmiClient = createClient({
+        autoConnect: true,
+        connectors,
+        provider,
+    });
+
+    return (
+        <WagmiConfig client={wagmiClient}>
+            <RainbowKitProvider chains={chains}>
+                <ConnectButton />
+            </RainbowKitProvider>
+        </WagmiConfig>
+    );
+}
+ 
+export default Wallet;
--- packages/react-app/src/pages/LandingPage.jsx
+++ packages/react-app/src/pages/LandingPage.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from '@emotion/styled';
+
+// Style
+const MyLink = styled(Link)`
+    font-size: 2.6rem;
+    text-decoration: none;
+    background-color: black;
+    padding: 1rem 2rem;
+    border-radius: .5rem;
+    margin-left: 2rem;
+    color: white;
+`;
 
 const LandingPage = () => {
     return (
-        <h1>From Landing Page</h1>
+        <div>
+            <h1>From Landing Page</h1>
+            <MyLink to='/new-nft'>
+                New NFT Page
+            </MyLink>
+        </div>
     );
 }
  
