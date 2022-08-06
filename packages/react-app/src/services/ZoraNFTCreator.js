95f1f1448576ff91471877acbd14ac8cf4df8884
Josep Chetrit <jchetrit1999@gmail.com>
2022-08-06 00:31:50 -0400

    Changing the script to store the videos using livepeer #2

--- packages/react-app/src/services/ZoraNFTCreator.js
+++ packages/react-app/src/services/ZoraNFTCreator.js
@@ -1,33 +1,56 @@
-import ZoraCreatorABI from '@zoralabs/nft-drop-contracts/dist/artifacts/ZoraNFTCreatorV1.sol/ZoraNFTCreatorV1.json';
-import { ethers } from 'ethers';
+import ZoraCreatorABI from "@zoralabs/nft-drop-contracts/dist/artifacts/ZoraNFTCreatorV1.sol/ZoraNFTCreatorV1.json";
+import { ethers } from "ethers";
 
-const ZoraNFTCreatorProxy_ADDRESS_RINKEBY = "0x2d2acD205bd6d9D0B3E79990e093768375AD3a30"
+const ZoraNFTCreatorProxy_ADDRESS_RINKEBY =
+  "0x2d2acD205bd6d9D0B3E79990e093768375AD3a30";
 
 const salesConfig = [
-    0.1,100,0,0,0,0,0x0000000000000000000000000000000000000000
-]
+  0.1, 100, 0, 0, 0, 0, 0x0000000000000000000000000000000000000000,
+];
 
 async function requestAccount() {
-    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  await window.ethereum.request({ method: "eth_requestAccounts" });
 }
 
 function newProvider() {
-    return new ethers.providers.Web3Provider(window.ethereum);
+  return new ethers.providers.Web3Provider(window.ethereum);
 }
 
 function instanceContract(address, abi, signer) {
-    return new ethers.Contract(address, abi, signer);
+  return new ethers.Contract(address, abi, signer);
 }
 
-export async function createEdition(name,symbol,editionSize,royaltyBPS,fundsRecipient,description,animationURI,imageURI) {
-    if (typeof window.ethereum !== 'undefined') {
-        await requestAccount();
-        const provider = newProvider();
-        const signer = provider.getSigner();
-        const contract = instanceContract(ZoraNFTCreatorProxy_ADDRESS_RINKEBY, ZoraCreatorABI, signer);
-        const tx = await contract.createEdition(name, symbol,editionSize,royaltyBPS,fundsRecipient,signer.address,salesConfig,description,animationURI,imageURI);
-        await tx.wait();
-    }
-
+export async function createEdition(
+  name,
+  symbol,
+  editionSize,
+  royaltyBPS,
+  fundsRecipient,
+  description,
+  animationURI,
+  imageURI
+) {
+  if (typeof window.ethereum !== "undefined") {
+    await requestAccount();
+    const provider = newProvider();
+    const signer = provider.getSigner();
+    const contract = instanceContract(
+      ZoraNFTCreatorProxy_ADDRESS_RINKEBY,
+      ZoraCreatorABI,
+      signer
+    );
+    const tx = await contract.createEdition(
+      name,
+      symbol,
+      editionSize,
+      royaltyBPS,
+      fundsRecipient,
+      signer.address,
+      salesConfig,
+      description,
+      animationURI,
+      imageURI
+    );
+    await tx.wait();
+  }
 }
-
--- packages/react-app/src/services/livepeerVideo.js
+++ packages/react-app/src/services/livepeerVideo.js
@@ -1,27 +1,18 @@
-import { videonft } from '@livepeer/video-nft'
-require("dotenv").config()
+import { videonft } from "@livepeer/video-nft";
 
-const apiOpts = {
-  auth: { apiKey: process.env.LIVEPEER },
-  // defaults to current origin if not specified
-  endpoint: videonft.api.prodApiEndpoint
-};
-const chainId = ethereum.chainId; // or await ethereum.request({ method: 'eth_chainId' });
-const minter = new videonft.minter.FullMinter(apiOpts, { ethereum, chainId });
-
-async function mintNft() {
-    const file = await minter.uploader.pickFile();
-    let asset = await minter.api.createAsset('My NFT', file);
-    // optional, optimizes the video for the NFT
-    asset = await minter.api.nftNormalize(asset);
-  
-    const nftMetadata = {
-      description: 'My NFT description',
-      traits: { 'my-custom-trait': 'my-custom-value' }
-    };
-    const ipfs = await minter.api.exportToIPFS(asset.id, nftMetadata);
-    // const tx = await minter.web3.mintNft(ipfs.nftMetadataUrl);
-    // const nftInfo = await minter.web3.getMintedNftInfo(tx);
-    // console.log(`minted NFT on contract ${nftInfo.contractAddress} with ID ${nftInfo.tokenId}`);
-    return ipfs;
-  }
+export async function storeVideo(file, descriptionMusic) {
+  const apiOpts = {
+    auth: { apiKey: process.env.LIVEPEER },
+    // defaults to current origin if not specified
+    endpoint: videonft.api.prodApiEndpoint,
+  };
+  const chainId = ethereum.chainId; // or await ethereum.request({ method: 'eth_chainId' });
+  const minter = new videonft.minter.FullMinter(apiOpts, { ethereum, chainId });
+  let asset = await minter.api.createAsset("My Music", file);
+  asset = await minter.api.nftNormalize(asset);
+  const nftMetadata = {
+    description: descriptionMusic,
+  };
+  const ipfs = await minter.api.exportToIPFS(asset.id, nftMetadata);
+  return ipfs;
+}
